54568b531051eacc0430cdb1ccbb2961b1654186
evgeniy_liubich <lyubitch89@yandex.by>
2020-06-04 00:43:39 +0300

    for heroku3

--- src/srv.py
+++ src/srv.py
@@ -10,32 +10,45 @@ now = datetime.now().year
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path.startswith("/hello"):
-            path, qs = self.path.split("?")
-            qs = parse_qs(qs)
-            if 'name' not in qs:
-                name = 'anonymous'
+        try:
+            if self.path.startswith("/hello"):
+                path, qs = self.path.split("?")
+                qs = parse_qs(qs)
+                if 'name' not in qs:
+                    name = 'anonymous'
+                else:
+                    name = qs["name"][0]
+                if 'age' not in qs:
+                    born = ''
+                else:
+                    age = qs['age'][0]
+                    year = now - int(age)
+                    born = 'You were born in ' + str(year) + ' year'
+                msg = f"""
+                    Hello {name}!
+                    {born}
+                    Your path:  {path}
+                """
+                self.send_response(200)
+                self.send_header("Content-type", "text/plain")
+                self.send_header("Content-length", str(len(msg)))
+                self.end_headers()
+                self.wfile.write(msg.encode())
+
             else:
-                name = qs["name"][0]
-            if 'age' not in qs:
-                born = ''
-            else:
-                age = qs['age'][0]
-                year = now - int(age)
-                born = 'You were born in ' + str(year) + ' year'
+                return SimpleHTTPRequestHandler.do_GET(self)
+        except ValueError:
             msg = f"""
-                Hello {name}!
-                {born}
-                Your path:  {path}
-            """
+                    Hello anonymous!
+                                
+                    {self.path}
+                    """
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.send_header("Content-length", str(len(msg)))
             self.end_headers()
             self.wfile.write(msg.encode())
 
-        else:
-            return SimpleHTTPRequestHandler.do_GET(self)
 
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
