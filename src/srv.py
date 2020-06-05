22331d06079a8949703c91435f9676d2bad36de5
evgeniy_liubich <lyubitch89@yandex.by>
2020-06-05 14:59:34 +0300

    new srv

--- src/srv.py
+++ src/srv.py
@@ -1,53 +1,53 @@
 import socketserver
 import os
 from http.server import SimpleHTTPRequestHandler
-from  urllib.parse import parse_qs
+from urllib.parse import parse_qs
 from datetime import datetime
 
 PORT = int(os.getenv('PORT', 8000))
 print(f'{PORT}')
 now = datetime.now().year
 
+
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        try:
-            if self.path.startswith("/hello"):
-                path, qs = self.path.split("?")
-                qs = parse_qs(qs)
-                if 'name' not in qs:
-                    name = 'anonymous'
-                else:
-                    name = qs["name"][0]
-                if 'age' not in qs:
-                    born = ''
-                else:
-                    age = qs['age'][0]
-                    year = now - int(age)
-                    born = 'You were born in ' + str(year) + ' year'
-                msg = f"""
+        if self.path.endswith("/hello") or self.path.endswith("/hello/"):
+            msg = f"""
+                Hello anonymous!
+
+                {self.path}
+                                """
+
+        elif self.path.startswith("/hello"):
+            path, qs = self.path.split("?")
+            qs = parse_qs(qs)
+            if 'name' not in qs:
+                name = 'anonymous'
+            else:
+               name = qs["name"][0]
+            if 'age' not in qs:
+                born = ''
+            else:
+                age = qs['age'][0]
+                year = now - int(age)
+                born = 'You were born in ' + str(year) + ' year'
+            msg = f"""
                     Hello {name}!
+                    {self.path.split("?")}
+                    {path}   {qs}
                     {born}
                     Your path:  {path}
                 """
-                self.send_response(200)
-                self.send_header("Content-type", "text/plain")
-                self.send_header("Content-length", str(len(msg)))
-                self.end_headers()
-                self.wfile.write(msg.encode())
+        else:
+            return SimpleHTTPRequestHandler.do_GET(self)
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", str(len(msg)))
+        self.end_headers()
+        self.wfile.write(msg.encode())
 
-            else:
-                return SimpleHTTPRequestHandler.do_GET(self)
-        except ValueError:
-            msg = f"""
-                    Hello anonymous!
-                                
-                    {self.path}
-                    """
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.send_header("Content-length", str(len(msg)))
-            self.end_headers()
-            self.wfile.write(msg.encode())
+
+
 
 
 
